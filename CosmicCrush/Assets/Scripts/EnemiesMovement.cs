8cf8552b43adb5e0920e23548babdc87d99882e1
MarieEliseGhijsen <marieeliseghijsen@yahoo.com>
2026-09-25 14:43:30 +0100

    collisions and changing materials

--- CosmicCrush/Assets/Scripts/EnemiesMovement.cs
+++ CosmicCrush/Assets/Scripts/EnemiesMovement.cs
@@ -1,27 +1,129 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class EnemiesMovement : MonoBehaviour
 {
     //public Transform player;
+    public float mass; //MIN : 10 ;;;; SMA : 0-100, MED : 100-200, LAR : 200-300
+
+    private Rigidbody rb;
     private Vector3 dir;
-    private float mass; //MIN : 10 ;;;; SMA : 0-100, MED : 100-200, LAR : 200-300
     private float speed;
+
+    private float timer = 0;
+
+    public Material small;
+    public Material med;
+    public Material big;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+
         dir.x = Random.Range(-1.0f, 1.0f);
         dir.y = 0.0f;
         dir.z = Random.Range(-1.0f, 1.0f);
 
-        mass = Random.Range(10.0f, 150.0f);
-        speed = 0.1f;
+        mass = Random.Range(0.1f, 2.0f);
+        //mass = 0.1f;
+        speed = 1.0f;
+
+        //small = Resources.Load("Assets\\Materials\\SmallEnemy.mat", typeof(Material)) as Material;
+        //med = Resources.Load("Assets\\Materials\\MediumEnemy.mat", typeof(Material)) as Material;
+        //big = Resources.Load("Assets\\Materials\\LargeEnemy.mat", typeof(Material)) as Material;
+
+        Debug.Log(small);
+        Debug.Log(med);
+        Debug.Log(big);
+
+        ColourSet();
+    }
+
+    private void ColourSet()
+    {
+        Debug.Log("COLOURRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR");
+
+        Renderer skin = GetComponent<Renderer>();
+        Material material = GetComponent<Renderer>().material;
+
+        if (mass >= 0.0f && mass < 1.0f)
+        {
+            skin.material = small;
+        }
+        else if (mass >= 1.0f && mass < 2.0f)
+        {
+            skin.material = med;
+        }
+        else if (mass >= 2.0f && mass < 3.0f)
+        {
+            skin.material = big;
+        }
+        else
+        {
+            mass = 2.9f;
+            ColourSet();
+            //skin.material.color = Color.blue;
+        }
+
+        Debug.Log(small);
+        Debug.Log(med);
+        Debug.Log(big);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (timer < 2.0f)
+        {
+            rb.AddForce(dir * speed);
+        }
+        else
+        {
+            dir.x = Random.Range(-1.0f, 1.0f);
+            dir.z = Random.Range(-1.0f, 1.0f);
+            timer = 0.0f;
+        }
 
+        timer = timer + 0.1f;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.CompareTag("Enemy"))
+        //{
+        //    Destroy(gameObject);
+
+        //    winTextObject.SetActive(true);
+        //    winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        //}
+
+        Debug.Log("enterde collisosin");
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            EnemiesMovement enemyScript = collision.gameObject.GetComponent<EnemiesMovement>();
+            Debug.Log("ENTERED COLLISON - TAG ENEMY");
+
+
+            if (enemyScript != null)
+            {
+                if (enemyScript.mass > mass)
+                {
+                    Debug.Log("ENEMY bigger than Enemy");
+                }
+                else
+                {
+                    Debug.Log("ENEMY smaller than Enemy");
+                    mass = mass + enemyScript.mass;
+                    Destroy(collision.gameObject);
+                    ColourSet();
+                }
+            }
+
+
+        }
     }
 }
--- CosmicCrush/Assets/Scripts/PlayerController.cs
+++ CosmicCrush/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,10 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro;
 using UnityEngine.UI;
+using static UnityEditor.ShaderData;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,15 +15,19 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private float movementX;
     private float movementY;
-    private int mass;
+    private float mass;
+    int score;
+
+    //private MonoBehaviour enemyScript = GetMonoBehaviour()
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        mass = 0;
+        mass = 0.5f;
+        score = 0;
 
-        SetMassText();
+        SetScoreText();
         winTextObject.SetActive(false);
     }
 
@@ -31,15 +38,13 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
-    void SetMassText()
+    void SetScoreText()
     {
-        massTextObject.GetComponent<TextMeshProUGUI>().text = "Mass : " + mass.ToString();
+        massTextObject.GetComponent<TextMeshProUGUI>().text = "Score : " + (score).ToString();
 
         if(mass >= 500)
         {
             winTextObject.SetActive(true);
-
-            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
         }
     }
 
@@ -52,23 +57,81 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        //if (collision.gameObject.CompareTag("Enemy"))
+        //{
+        //    Destroy(gameObject);
+
+        //    winTextObject.SetActive(true);
+        //    winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        //}
+
+        //Debug.Log("enterde collisosin");
+
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Destroy(gameObject);
+            EnemiesMovement enemyScript = collision.gameObject.GetComponent<EnemiesMovement>();
+            //Debug.Log("ENTERED COLLISON - TAG ENEMY");
 
-            winTextObject.SetActive(true);
-            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+            if (enemyScript != null)
+            {
+                if (enemyScript.mass > mass)
+                {
+                    //Debug.Log("ENEMY bigger");
+                    winTextObject.SetActive(true);
+                    winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+                }
+                else
+                {
+                    //Debug.Log("ENEMY smaller");
+                    mass = mass + enemyScript.mass;
+                    score = score + (int)(enemyScript.mass * 100.0f);
+                    SetScoreText();
+                    Destroy(collision.gameObject);
+                }
+            }
+
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Pickup"))
-        {
-            other.gameObject.SetActive(false);
-            mass = mass + 1;
+        //Destroy(other);
 
-            SetMassText();
-        }
+        //if (other.gameObject.CompareTag("Pickup"))
+        //{
+        //    other.gameObject.SetActive(false);
+        //    mass = mass + 1;
+
+        //    SetScoreText();
+        //}
+
+        //Debug.Log("enterde collisosin");
+
+        //if (other.gameObject.CompareTag("Enemy"))
+        //{
+        //    EnemiesMovement enemyScript = other.gameObject.GetComponent<EnemiesMovement>();
+        //    Debug.Log("ENTERED COLLISON - TAG ENEMY");
+
+
+        //    if (enemyScript != null)
+        //    {
+        //        if (enemyScript.mass < mass)
+        //        {
+        //            Debug.Log("ENEMY bigger");
+        //            winTextObject.SetActive(true);
+        //            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        //        }
+        //        else
+        //        {
+        //            Debug.Log("ENEMY smaller");
+        //            mass = mass + enemyScript.mass;
+        //            score = score + (int)(enemyScript.mass * 100.0f);
+        //            SetScoreText();
+        //            Destroy(other);
+        //        }
+        //    }
+
+
+        //}
     }
 }
